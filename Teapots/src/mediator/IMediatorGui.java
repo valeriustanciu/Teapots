9d9e7173a1c63d69f17d9e4e38c26ad932474e49
Andreea Mindroiu <andreea.mindroiu@gmail.com>
2013-03-23 22:26:16 +0200

    added methods

--- Teapots/src/mediator/IMediatorGui.java
+++ Teapots/src/mediator/IMediatorGui.java
@@ -9,4 +9,9 @@ public interface IMediatorGui {
 
 	public String getUserType(String username, String password);
 	public ArrayList<String> getUserServices (String user);
+	
+	public void changeState (String newState);
+	
+	public void addService (String user, String service);
+	public void removeService (String user, String service);
 }
