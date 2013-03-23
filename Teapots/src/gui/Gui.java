663e81114b750c05872539f6e9bb731f5c31e5c7
Andreea Mindroiu <andreea.mindroiu@gmail.com>
2013-03-23 22:16:25 +0200

    modified auctiontable

--- Teapots/src/gui/Gui.java
+++ Teapots/src/gui/Gui.java
@@ -2,21 +2,30 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JPopupMenu;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import mediator.IMediatorGui;
 import mediator.Mediator;
@@ -27,7 +36,7 @@ public class Gui extends JPanel implements IGui{
 	private JPanel auctionScreen;
 	private JTextField user = new JTextField();
 	private JPasswordField pass = new JPasswordField();
-	private DefaultTableModel model;
+	private MyDefaultTableModel model;
 	private String userName;
 	private String userType;
 	private ArrayList<String> userServices;
@@ -92,7 +101,7 @@ public class Gui extends JPanel implements IGui{
 	public void auctionGui() {
 		this.remove(loginScreen);
 		JPanel auctionScreen = new JPanel();
-		model = new DefaultTableModel();
+		model = new MyDefaultTableModel();
 		this.setLayout(new BorderLayout());
 		
 		JPanel topPanel = new JPanel(new GridLayout(1, 2));
@@ -117,11 +126,74 @@ public class Gui extends JPanel implements IGui{
 		userServices = mediator.getUserServices(userName);
 		
 		for(int i = 0; i < userServices.size(); i++) {
-			Object[] rowParams = {userServices.get(i), "inactive", null, null};
+			Object[] rowParams = {userServices.get(i), "inactive", new JComboBox(), new JProgressBar()};
 			model.addRow(rowParams);
 		}
 		
 		auctionTable = new JTable(model);
+		auctionTable.setCellSelectionEnabled(true);
+
+		
+		TableColumn col = auctionTable.getColumnModel().getColumn(2);
+		MyTableCellRenderer customRenderer = new MyTableCellRenderer();
+		col.setCellRenderer(customRenderer);
+				
+		col = auctionTable.getColumnModel().getColumn(3);
+		col.setCellRenderer(customRenderer);
+		
+		auctionTable.addMouseListener(new MouseAdapter () {
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e)) {
+					// select the cell before expanding popup menu
+					Point p = e.getPoint();
+					final int rowNumber = auctionTable.rowAtPoint(p);
+					final int columnNumber = auctionTable.columnAtPoint(p);
+					ListSelectionModel lmodel = auctionTable.getSelectionModel();
+
+					//services column for buyers
+					if (columnNumber == 0 && userType.equals("buyer")) {
+			    		JPopupMenu jpop = new JPopupMenu();
+			    		JMenuItem it1 = new JMenuItem("Launch offer request");
+			    		JMenuItem it2 = new JMenuItem("Drop offer request");
+			    		it1.addActionListener(new CustomActionListener(auctionTable));
+			    		it2.addActionListener(new CustomActionListener(auctionTable));
+			    		jpop.add(it1);
+			    		jpop.add(it2);
+			    		add(jpop);
+			    		jpop.show(e.getComponent(), e.getX(), e.getY());
+						lmodel.setSelectionInterval(rowNumber, rowNumber);
+					}
+					
+					//users column for buyers
+					if (columnNumber == 2 && userType.equals("buyer")) {
+			    		JPopupMenu jpop = new JPopupMenu();
+			    		JMenuItem it1 = new JMenuItem("Accept offer");
+			    		JMenuItem it2 = new JMenuItem("Refuse offer");
+			    		it1.addActionListener(new CustomActionListener(auctionTable));
+			    		it2.addActionListener(new CustomActionListener(auctionTable));
+			    		jpop.add(it1);
+			    		jpop.add(it2);
+			    		add(jpop);
+			    		jpop.show(e.getComponent(), e.getX(), e.getY());
+						lmodel.setSelectionInterval(rowNumber, rowNumber);						
+					}
+					
+					//users column for sellers
+					if (columnNumber == 2 && userType.equals("seller")) {
+			    		JPopupMenu jpop = new JPopupMenu();
+			    		JMenuItem it1 = new JMenuItem("Make offer");
+			    		JMenuItem it2 = new JMenuItem("Drop auction");
+			    		it1.addActionListener(new CustomActionListener(auctionTable));
+			    		it2.addActionListener(new CustomActionListener(auctionTable));
+			    		jpop.add(it1);
+			    		jpop.add(it2);
+			    		add(jpop);
+			    		jpop.show(e.getComponent(), e.getX(), e.getY());
+						lmodel.setSelectionInterval(rowNumber, rowNumber);
+					}
+				}
+			}
+		});
 		auctionScreen.add(new JScrollPane(auctionTable));
 		this.add(auctionScreen, BorderLayout.CENTER);
 		this.add(topPanel, BorderLayout.NORTH);
